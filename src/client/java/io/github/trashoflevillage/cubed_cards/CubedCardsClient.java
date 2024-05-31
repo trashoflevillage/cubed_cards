c717740dc2a5d9f97a8c15cae97c2e7dd0341290
trashoflevillage <uhhnokthxbye@gmail.com>
2024-05-31 17:21:10 -0400

    Initial commit

---
+++ src/client/java/io/github/trashoflevillage/cubed_cards/CubedCardsClient.java
@@ -0,0 +1,10 @@
+package io.github.trashoflevillage.cubed_cards;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CubedCardsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
